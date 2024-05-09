fd9c7b1dd48a46fbcb572d794b3f640ecb6d2317
Shershen Alexandra <hornet240204@gmail.com>
2024-05-09 12:06:43 +0300

    Remove all()

--- taxi/views.py
+++ taxi/views.py
@@ -18,7 +18,7 @@ def index(request):
 
 class ManufacturerListView(ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.order_by("name")
     paginate_by = 5
 
 
@@ -40,4 +40,4 @@ class DriverListView(ListView):
 
 class DriverDetailView(DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars")
+    queryset = Driver.objects.prefetch_related("cars")
